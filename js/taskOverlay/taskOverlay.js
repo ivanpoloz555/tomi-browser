minbrowser/min

commit 52141760c0cff7500805419431f29464c1cefbdf

empty the task container when the overlay is closed

js/taskOverlay/taskOverlay.js
@@ -67,7 +67,6 @@ var taskOverlay = {
     taskSwitcherButton.classList.add('active')
 
     this.tabDragula.containers = []
-    empty(taskContainer)
 
     // show the task elements
     tasks.get().forEach(function (task, index) {
@@ -94,6 +93,11 @@ var taskOverlay = {
     if (this.isShown) {
       this.isShown = false
       this.overlayElement.hidden = true
+
+      //wait until the animation is complete to remove the tab elements
+      setTimeout(function () {
+        empty(taskContainer)
+      }, 200);
 
       document.body.classList.remove('task-overlay-is-shown')
 

ivanpoloz555/tomi-browser

commit 169691c76886082ff34fc11cd91b2c1814599690

fix bugs introduced by 52141760c0cff7500805419431f29464c1cefbdf
sometimes, the task elements would appear multiple times or not appear
at all

js/taskOverlay/taskOverlay.js
@@ -67,6 +67,7 @@ var taskOverlay = {
     taskSwitcherButton.classList.add('active')
 
     this.tabDragula.containers = []
+    empty(taskContainer)
 
     // show the task elements
     tasks.get().forEach(function (task, index) {
@@ -96,7 +97,9 @@ var taskOverlay = {
 
       //wait until the animation is complete to remove the tab elements
       setTimeout(function () {
-        empty(taskContainer)
+        if (!taskOverlay.isShown) {
+          empty(taskContainer)
+        }
       }, 200);
 
       document.body.classList.remove('task-overlay-is-shown')